1ecad1949749fe8745b303d116796d5155b9d14f
Daniel Briechle <danbriechle@hotmail.com>
2019-02-12 20:20:43 -0700

    basic request complete

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -4,10 +4,14 @@ class DarkSkyService
     ENV['DARK_SKY_API_KEY']
   end
 
-  def self.conn(lat_lon)
-  @conn =  Faraday.new(url: "https://api.darksky.net") do |f|
+  def self.conn
+    Faraday.new(url: "https://api.darksky.net") do |f|
       f.adapter Faraday.default_adapter
     end
-    thing = @conn.get "/forecast/#{key}/#{lat_lon}"
   end
+
+  def self.forecast(lat_lon)
+    conn.get "/forecast/#{key}/#{lat_lon}"
+  end
+
 end
--- spec/service/dark_sky_service_spec.rb
+++ spec/service/dark_sky_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Dark Sky Api' do
   it 'can connect' do
     lat_lon = "37.8267,-122.4233"
-    response = DarkSkyService.conn(lat_lon)
+    response = DarkSkyService.forecast(lat_lon)
 
     expect(response.status).to eq(200)
   end
