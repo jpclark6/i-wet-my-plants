b362c979bb861aabdcf370dab7dd8e82a46055ce
jpclark6 <jpclark6@gmail.com>
2019-02-12 19:53:02 -0700

    finishes finding lat long from zip

--- spec/services/google_geocode_spec.rb
+++ spec/services/google_geocode_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 describe 'with a zip code' do
   it 'can send back lat long' do
-    zip = 80003
+    zip = 80204
     geo_service = GoogleGeocodeService.new(zip)
-    expect(geo_service.lat).to eq(39.8182494)
-    expect(geo_service.lng).to eq(-105.0674317)
+    expect(geo_service.lat).to eq(39.7380371)
+    expect(geo_service.lng).to eq(-105.0265195)
+  end
+
+  it 'can send back a different lat long' do
+    zip = 84103
+    geo_service = GoogleGeocodeService.new(zip)
+    expect(geo_service.lat).to eq(40.810506)
+    expect(geo_service.lng).to eq(-111.8449346)
   end
 end
