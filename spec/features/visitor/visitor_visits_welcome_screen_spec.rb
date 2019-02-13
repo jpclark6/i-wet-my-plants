078fb080abb3c61f2ecdc04c836918c19d946510
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-12 20:57:34 -0700

    Updates test to expect link instead of button.

--- spec/features/visitor/visitor_visits_welcome_screen_spec.rb
+++ spec/features/visitor/visitor_visits_welcome_screen_spec.rb
@@ -4,12 +4,9 @@ describe 'as a visitor on the welcome screen' do
   it 'shows link to sign up or register an account' do
     visit '/'
 
-    expect(page).to have_content("I Wet My Plants is an app that helps you
-                                  remember to water the plants in your garden,
-                                  and send you a friendly reminder if you forget.
-                                  ")
+    expect(page).to have_content("I Wet My Plants is an app that helps you remember to water the plants in your garden, and send you a friendly reminder if you forget.")
 
-    expect(page).to have_button("Register")
-    expect(page).to have_button("Sign-In")
+    expect(page).to have_link("Register")
+    expect(page).to have_link("Sign-In")
   end
 end
