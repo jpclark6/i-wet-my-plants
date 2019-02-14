754819a93737d192cfebaac07aaaac6c69dde0c8
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-13 20:25:40 -0700

    changes google api key ENV method to be standardized to all caps & include api & key in the name.

--- app/services/google_geocode_service.rb
+++ app/services/google_geocode_service.rb
@@ -12,7 +12,7 @@ class GoogleGeocodeService
   end
 
   def location_data
-    get_json("/maps/api/geocode/json?address=#{@zip}") 
+    get_json("/maps/api/geocode/json?address=#{@zip}")
   end
 
   private
@@ -25,7 +25,7 @@ class GoogleGeocodeService
 
   def conn
     Faraday.new(url: 'https://maps.googleapis.com') do |faraday|
-      faraday.params['key'] = ENV['google_api']
+      faraday.params['key'] = ENV['GOOGLE_API_KEY']
       faraday.adapter Faraday.default_adapter
     end
   end
