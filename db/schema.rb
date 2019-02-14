a3ee5cc75d05187e9fcd99491a75b16f1f99c50f
Daniel Briechle <danbriechle@hotmail.com>
2019-02-13 22:49:16 -0700

    adds password digest to users model adds bcrypt gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_052604) do
+ActiveRecord::Schema.define(version: 2019_02_14_054743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_02_14_052604) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "password"
+    t.string "password_digest"
   end
 
   add_foreign_key "plants", "gardens"
