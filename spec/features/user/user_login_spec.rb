88ac7062202711a44447180aefe397315b131caa
Daniel Briechle <danbriechle@hotmail.com>
2019-02-13 23:03:34 -0700

    adds test that user can log out

--- spec/features/user/user_login_spec.rb
+++ spec/features/user/user_login_spec.rb
@@ -18,5 +18,27 @@ describe 'As a visitor to the site' do
       expect(current_path).to eq(dashboard_path)
       expect(page).to have_content("Logged In Successfully")
     end
+
+    it 'should let me log out'
+    it 'should let me log in' do
+      user = User.create(name: "dave", zip_code: "80203", email: 'email@aol.com', password: 'password')
+      user.gardens << Garden.create(name: 'Backyard')
+      user.gardens.first.plants << plant_1 = Plant.new(name: 'Alice', species: 'Rose', frequency: 24)
+      user.gardens.first.plants << plant_2 = Plant.new(name: 'Tom', species: 'Carrot', frequency: 12)
+      user.gardens.first.plants << plant_3 = Plant.new(name: 'Elbert', species: 'Beet', frequency: 18)
+
+      visit login_path
+
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
+      click_button 'Log In'
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Logged In Successfully")
+
+      click_on "Logout"
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("You have logged out")
+    end
   end
 end
