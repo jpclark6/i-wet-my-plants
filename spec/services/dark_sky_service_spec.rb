abf313c5c514a566bcf69563ce23acc585c0fa82
Daniel Briechle <danbriechle@hotmail.com>
2019-02-13 22:07:15 -0700

    adds initial test for hook up

--- spec/services/dark_sky_service_spec.rb
+++ spec/services/dark_sky_service_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe 'Dark Sky Api' do
   it 'can connect' do
-    lat_lon = "37.8267,-122.4233"
-    response = DarkSkyService.forecast(lat_lon)
+    zip = 80204
+
+    response = DarkSkyService.forecast(zip)
 
     expect(response.status).to eq(200)
   end
