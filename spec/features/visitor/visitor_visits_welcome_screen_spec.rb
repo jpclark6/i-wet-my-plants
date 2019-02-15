a11536cff12d804dc7a84b3b57c6e3a273c28df1
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-14 22:27:14 -0700

    Updates name of the it block for the vistor goes to welcome page test.

--- spec/features/visitor/visitor_visits_welcome_screen_spec.rb
+++ spec/features/visitor/visitor_visits_welcome_screen_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'as a visitor on the welcome screen' do
-  it 'shows link to sign up or register an account' do
+  it 'shows link to login with facebook to signin or create an account' do
     visit '/'
 
     expect(page).to have_content("I Wet My Plants is an app that helps you remember to water the plants in your garden, and send you a friendly reminder if you forget.")
