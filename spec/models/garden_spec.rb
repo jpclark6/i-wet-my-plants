26c76841d3c8222547d20c92ad3cd6a75695ff70
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-15 18:00:34 -0700

    Adds garden model test validation for twitter_handle & zip_code fields.

--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 describe Garden do
   describe 'validations' do
     it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:twitter_handle) }
+    it { should validate_presence_of(:zip_code) }
 
   end
   describe 'relationships' do
     it { should belong_to(:user) }
+    it { should have_many(:plants) }
   end
 end
