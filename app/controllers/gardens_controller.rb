8d99197b0a9a00b793e82c939590343e689d64b2
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-15 17:59:44 -0700

    Gets new & create methods to work in the garden controller.

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -4,7 +4,12 @@ class GardensController < ApplicationController
   end
 
   def create
-    @garden = Garden.create(params)
+
+    @garden = Garden.new(name: garden_params[:name],
+                            twitter_handle: garden_params[:twitter_handle],
+                            zip_code: garden_params[:zip_code],
+                            user_id: current_user.id)
+
     if @garden.save
       redirect_to plants_path
       flash[:success] = "Your garden has been created."
@@ -12,5 +17,11 @@ class GardensController < ApplicationController
       render :new
       flash[:error] = "ERROR: Your garden has not been created."
     end
+  end
+
+  private
+
+  def garden_params
+    params.require(:garden).permit(:name, :twitter_handle, :zip_code)
   end
 end
