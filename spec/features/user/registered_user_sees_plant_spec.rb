9897b8d4967570db4b65ac22e8e3de1d8fff3401
Maddie Jones <maddyg91@gmail.com>
2019-02-16 17:29:23 -0700

    Rebase fixes

--- spec/features/user/registered_user_sees_plant_spec.rb
+++ spec/features/user/registered_user_sees_plant_spec.rb
@@ -28,11 +28,12 @@ describe 'as a registered user' do
     plant_1 = garden.plants.create(name: 'Alice', species: 'Rose', frequency: 24, last_watered: Time.now)
     plant_2 = garden.plants.create(name: 'Tom', species: 'Carrot', frequency: 12, last_watered: "2019-02-09")
     # As a registered user.
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
     visit '/plants'
     # When I visit my garden,
     within('.menu') do
       expect(page).to have_link('Garden')
-      expect(page).to have_link('Water All Plants')
+      expect(page).to have_button('Water All Plants')
       expect(page).to have_link('Logout')
     end
     within('.garden') do
@@ -59,26 +60,17 @@ describe 'as a registered user' do
   end
 
   it 'it cannot create a plant with bad info' do
-    user_1 = User.create!(name: "Bobby", uid: '49j8jesj')
-    garden = Garden.create(name: 'Backyard', user: user_1, zip_code: 84928)
-    garden.plants << plant_1 = Plant.create(name: 'Alice', species: 'Rose', frequency: 24, last_watered: Time.now)
-    garden.plants << plant_2 = Plant.create(name: 'Tom', species: 'Carrot', frequency: 12, last_watered: Time.now)
-    garden.plants << plant_3 = Plant.create(name: 'Elbert', species: 'Beet', frequency: 18, last_watered: Time.now)
+    user_1 = User.create(name: "Bobby", uid: '49j8jesj')
+    garden = Garden.create(name: 'Backyard', user: user_1, zip_code: 84928, twitter_handle: "Maddie")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
     # As a registered user.
     visit '/plants'
-    # When I visit my garden,
-    within('.menu') do
-      expect(page).to have_link('Garden')
-      expect(page).to have_link('Water All Plants')
-      expect(page).to have_link('Logout')
-    end
+
     within('.garden') do
       expect(page).to have_link('Add Plant')
       click_on 'Add Plant'
     end
-    # I should see a "Add plant" link,
 
     name = 'dave'
     frequency = '5'
@@ -89,32 +81,19 @@ describe 'as a registered user' do
     click_on 'Create Plant'
     #I should click "Create plant"
     expect(page).to have_content("species can't be blank")
-    # As registered user.
-    visit plant_path(plant_1.id)
-    # When I visit 'pants/:id'
-    # I see the plant name, watering history, a place to leave notes.
-    expect(page).to have_content("Name: #{plant_1.name}")
-    expect(page).to have_content("Last Watered: #{plant_1.last_watered.to_date}")
-    expect(page).to_not have_content("Name: #{plant_2.name}")
-    expect(page).to_not have_content("Last Watered: #{plant_2.last_watered.to_date}")
-
-    expect(page).to have_button("Edit Plant")
-    # I see "Edit Plant"
-    expect(page).to have_button("Kill Me")
-    # and "Kill Me" buttons
   # I see a message "Incorrect Info"
  end
  it 'can edit plant' do
    user_1 = User.create(name: "Bobby", uid: '49j8jesj')
-   garden = Garden.create(name: 'Backyard', user: user_1, zip_code: 84928)
+   garden = Garden.create(name: 'Backyard', user: user_1, zip_code: 84928, twitter_handle: 'Maddie')
    plant_1 = Plant.create(name: 'Alice', species: 'Rose', frequency: 24, last_watered: Time.now, garden: garden)
    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
    visit '/plants'
 
-   expect(page).to have_link('Edit Plant')
+   expect(page).to have_link('Edit')
 
-   click_on 'Edit Plant'
+   click_on 'Edit'
 
    expect(current_path).to eq(edit_plant_path(plant_1))
 
