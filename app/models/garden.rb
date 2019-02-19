c92bfffe24cda8fe8a4dd749fbd642bade6cbe6b
jpclark6 <jpclark6@gmail.com>
2019-02-18 19:11:01 -0700

    styles page

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -6,6 +6,10 @@ class Garden < ApplicationRecord
   has_many :plants
 
   def plants_by_water_need
-    plants.select("plants.*, (EXTRACT(EPOCH FROM ((NOW()) - (last_watered))) - (frequency - 6) * 3600) as needing_water").order('needing_water desc')
+    unless plants.empty? 
+      plants.select("plants.*, (EXTRACT(EPOCH FROM ((NOW()) - (last_watered))) - (frequency - 6) * 3600) as needing_water").order('needing_water desc')
+    else
+      []
+    end
   end
 end
