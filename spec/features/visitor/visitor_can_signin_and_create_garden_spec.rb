9c7eb6b32af478e0baa2eba5acdc12e0474f3ef7
jpclark6 <jpclark6@gmail.com>
2019-02-19 11:44:11 -0700

    updates twitter tweeter test with vcr

--- spec/features/visitor/visitor_can_signin_and_create_garden_spec.rb
+++ spec/features/visitor/visitor_can_signin_and_create_garden_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 describe 'As a first time visitor to the site who signs in', :vcr do
   it 'I can create a garden' do
+    name = "Dave's Garden"
+    twitter_handle = "iwetmyplants1"
+    zip_code = 80203
+    
     user_1 = create(:user)
+    Garden.create(name: name, twitter_handle: twitter_handle, zip_code: zip_code, user: user_1)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
     visit '/gardens/new'
 
-    name = "Dave's Garden"
-    twitter_handle = "iwetmyplants1"
-    zip_code = 80203
 
     fill_in :garden_name, with: name
     fill_in :garden_twitter_handle, with: twitter_handle
@@ -19,7 +21,6 @@ describe 'As a first time visitor to the site who signs in', :vcr do
 
     expect(current_path).to eq('/plants')
 
-    user_1 = User.first
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
     expect(page).to have_content(name)
   end
