23013b47d1be5d043c6ce7b3cac4d77011cb945f
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-20 15:09:29 -0700

    Updates feature test for if raining, watering is reset, and adds model tests for helper services and methods.

--- spec/facades/dark_sky_facade_spec.rb
+++ spec/facades/dark_sky_facade_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 describe 'Dark Sky Facade' do
-  xit 'can find daily percipitation' do
-    #this test depends on currently raining and so it is not a good test
+  it 'can find current percipitation', :vcr do
     zip_code = "80026"
-    forecast = DarkSkyFacade.raining?(zip_code)
+    allow(DarkSkyFacade).to receive(:current_precip_probability).with(zip_code).and_return(0.8)
+    precipitation = DarkSkyFacade.current_precip_probability(zip_code)
 
-    expect(forecast).to eq(true)
+    expect(precipitation).to eq(0.8)
+  end
+
+  it 'can tell you if its raining', :vcr do
+    zip_code = "80026"
+    allow(DarkSkyFacade).to receive(:raining?).with(zip_code).and_return(true)
+    raining = DarkSkyFacade.raining?(zip_code)
+
+    expect(raining).to eq(true)
   end
 end
--- spec/features/rain_causes_watering_times_to_rest_spec.rb
+++ spec/features/rain_causes_watering_times_to_rest_spec.rb
@@ -1,22 +1,33 @@
 require 'rails_helper'
 
 describe 'as a resgistered user on the garden' do
-  xit 'changes watering times if raining', :vcr do
+  it 'changes watering times if raining', :vcr do
+    yesterday = 1.day.ago
+    two_days_ago = 2.days.ago
+    zip_code = 80203
     user_1 = create(:user)
-    garden = create(:garden, user: user_1)
-    plant_1 = create(:plant, name: 'Alice', species: 'Rose', frequency: 24, garden: garden)
-    plant_2 = create(:plant, name: 'Tom', species: 'Carrot', frequency: 12, garden: garden)
-    plant_3 = create(:plant, name: 'Elbert', species: 'Beet', frequency: 18, garden: garden)
-    lat_lon = "37.8267,-122.4233"
-    DarkSkyService.forecast(lat_lon)
+    garden = create(:garden, name: "justin_test", zip_code: zip_code, user: user_1)
+    plant_1 = create(:plant, garden: garden, frequency: 24, last_watered: two_days_ago)
+    plant_2 = create(:plant, garden: garden, frequency: 12, last_watered: yesterday)
+    plant_3 = create(:plant, garden: garden, frequency: 18, last_watered: yesterday)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
-    visit '/garden'
+    visit '/plants'
 
     within(all('.plant').first) do
-      expect(plant_1.hours_until_watering).to eq(1)
-      expect(plant_1.hours_until_watering).to_not eq(1)
+      expect(Garden.find(garden[:id]).plants[0].hours_until_watering).to eq(-24)
+      expect(Garden.find(garden[:id]).plants[0].hours_until_watering).to_not eq(24)
+    end
+
+    allow(DarkSkyFacade).to receive(:current_precip_probability).with(zip_code).and_return(0.9)
+    CurrentPrecipitationService.new.get_precipitation
+
+    visit '/plants'
+
+    within(all('.plant').first) do
+      expect(Garden.find(garden[:id]).plants[0].hours_until_watering).to eq(24)
+      expect(Garden.find(garden[:id]).plants[0].hours_until_watering).to_not eq(-24)
     end
   end
 end
