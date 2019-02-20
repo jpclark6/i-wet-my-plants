916fe45ed53401617cc078c507b98bc7401598c4
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-20 15:11:34 -0700

    Adds helper methods to reset last_watered field for all plants on a garden, and sets zipcode field to only be 5 digits.

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -4,13 +4,18 @@ class Garden < ApplicationRecord
                         :zip_code
   belongs_to :user
   has_many :plants
+  validates_length_of :zip_code, :maximum => 5
 
   def plants_by_water_need
-    unless plants.empty? 
+    unless plants.empty?
       plants.select("plants.*, (EXTRACT(EPOCH FROM ((NOW()) - (last_watered))) - (frequency - 6) * 3600) as needing_water")
             .order('needing_water desc')
     else
       []
     end
+  end
+
+  def reset_watering
+    plants.update_all(last_watered: Time.now)
   end
 end
