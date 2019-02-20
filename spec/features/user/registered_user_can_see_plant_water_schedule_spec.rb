03bcc1851956e059c9a616fa31d2c7ed2b42de21
Maddie Jones <maddyg91@gmail.com>
2019-02-20 10:21:26 -0700

    Adds test for waterings showing up in the plant show page

--- spec/features/user/registered_user_can_see_plant_water_schedule_spec.rb
+++ spec/features/user/registered_user_can_see_plant_water_schedule_spec.rb
@@ -17,26 +17,30 @@ describe 'as a registered user' do
       expect(plant.waterings.count).to eq(1)
     end
   end
-  describe 'when I visit any page' do
-    it 'and I click water all, waterings should be created for each plant', :vcr do
+  describe 'when I visit a plant show page' do
+    it 'should show watering schedule', :vcr do
       user = create(:user)
       garden = create(:garden, user: user)
       plant_1 = create(:plant, garden: garden)
-      plant_2 = create(:plant, garden: garden)
-      plant_3 = create(:plant, garden: garden)
+      watering_1 = create(:watering, plant: plant_1, created_at: 2.days.ago )
+      watering_2 = create(:watering, plant: plant_1, created_at: 1.day.ago)
+      watering_3 = create(:watering, plant: plant_1, created_at: 7.days.ago)
+      watering_4 = create(:watering, plant: plant_1, created_at: 6.days.ago)
+      watering_5 = create(:watering, plant: plant_1, created_at: 8.days.ago)
+      watering_6 = create(:watering, plant: plant_1, created_at: 5.days.ago)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      visit plants_path
+      visit plant_path(plant_1)
 
-      expect(plant_1.waterings.count).to eq(0)
-      expect(plant_2.waterings.count).to eq(0)
-      expect(plant_3.waterings.count).to eq(0)
-
-      click_on "Water All"
-
-      expect(plant_1.waterings.count).to eq(1)
-      expect(plant_2.waterings.count).to eq(1)
-      expect(plant_3.waterings.count).to eq(1)
+      expect(page).to have_css('.waterings')
+      within '.waterings' do
+        expect(page).to have_content(watering_1.created_at)
+        expect(page).to have_content(watering_2.created_at)
+        expect(page).to have_content(watering_3.created_at)
+        expect(page).to have_content(watering_4.created_at)
+        expect(page).to have_content(watering_5.created_at)
+        expect(page).to have_content(watering_6.created_at)
+      end
     end
   end
 end
