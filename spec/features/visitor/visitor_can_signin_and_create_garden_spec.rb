8ebc2cf94af4fb829b9838f3f7c02d698f7389b6
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-20 21:02:44 -0700

    Creates feature test for not creating a second garden & updates visitor signin for first time test.

--- spec/features/visitor/visitor_can_signin_and_create_garden_spec.rb
+++ spec/features/visitor/visitor_can_signin_and_create_garden_spec.rb
@@ -5,23 +5,19 @@ describe 'As a first time visitor to the site who signs in', :vcr do
     name = "Dave's Garden"
     twitter_handle = "iwetmyplants1"
     zip_code = 80203
-    
     user_1 = create(:user)
-    Garden.create(name: name, twitter_handle: twitter_handle, zip_code: zip_code, user: user_1)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
     visit '/gardens/new'
 
-
     fill_in :garden_name, with: name
     fill_in :garden_twitter_handle, with: twitter_handle
-
     fill_in :garden_zip_code, with: zip_code
     click_on 'Create Garden'
+    save_and_open_page
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
     expect(current_path).to eq('/plants')
-
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
     expect(page).to have_content(name)
   end
 end
