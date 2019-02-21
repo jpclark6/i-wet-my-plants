8fe6206704a0e07176b87dd4e4fdf1f0bbd1cbbe
Justin Mauldin <justin.mauldin7@gmail.com>
2019-02-21 11:10:32 -0700

    Updates feature test for signin to implement omniauth login flow.

--- spec/features/visitor/visitor_can_signin_and_create_garden_spec.rb
+++ spec/features/visitor/visitor_can_signin_and_create_garden_spec.rb
@@ -1,21 +1,17 @@
 require 'rails_helper'
 
-describe 'As a first time visitor to the site who signs in', :vcr do
-  it 'I can create a garden' do
+describe 'As a first time visitor to the site who signs in' do
+  it 'I can create a garden', :vcr do
+    visit '/'
+    click_on "Login with Facebook"
+
     name = "Dave's Garden"
     twitter_handle = "iwetmyplants1"
     zip_code = 80203
-    user_1 = create(:user)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
-
-    visit '/gardens/new'
-
     fill_in :garden_name, with: name
     fill_in :garden_twitter_handle, with: twitter_handle
     fill_in :garden_zip_code, with: zip_code
     click_on 'Create Garden'
-    save_and_open_page
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user_1)
 
     expect(current_path).to eq('/plants')
     expect(page).to have_content(name)
