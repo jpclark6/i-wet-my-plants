8902a042c6962746917b78b5c8a455afcc6d9ad9
jpclark6 <jpclark6@gmail.com>
2019-02-21 12:06:20 -0700

    refactors facades and services

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -1,20 +1,24 @@
 class DarkSkyService
-
   def self.key
     ENV['DARK_SKY_API_KEY']
+  end
+
+  def self.forecast(zip)
+    geo_service = GoogleGeocodeService.new(zip)
+    get_json("/forecast/#{key}/#{geo_service.lat},#{geo_service.lng}")
+  end
+
+  private
+
+  def self.get_json(url)
+    response = conn.get(url)
+
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.conn
     Faraday.new(url: "https://api.darksky.net") do |f|
       f.adapter Faraday.default_adapter
     end
-  end
-
-  def self.forecast(zip)
-    geo_service = GoogleGeocodeService.new(zip)
-    lat = geo_service.lat
-    lng = geo_service.lng
-    response = conn.get "/forecast/#{key}/#{lat},#{lng}"
-    JSON.parse(response.body, symbolize_names: true)
   end
 end
--- app/services/twitter_tweeter_service.rb
+++ app/services/twitter_tweeter_service.rb
@@ -1,13 +1,4 @@
 class TwitterTweeterService
-  def tweet
-    Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['TWITTER_API']
-      config.consumer_secret     = ENV['TWITTER_SECRET_API']
-      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
-    end
-  end
-
   def send_tweet(message)
     tweet.update(message)
   end
@@ -29,5 +20,16 @@ class TwitterTweeterService
     ["Hey it's #{plant.name}, your #{plant.species}, please water me!",
     "...it's #{plant.name}, your #{plant.species}...need...water...",
     "I could sure use some water! Love, your #{plant.species}, #{plant.name}"]
+  end
+
+  private 
+
+  def tweet
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_API']
+      config.consumer_secret     = ENV['TWITTER_SECRET_API']
+      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+    end
   end
 end
